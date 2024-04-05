39d5ed56679b2949e03d4589f2dcc92817e99668
Nhan Vo Quang <vqnhan@tma.com.vn>
2024-04-05 15:50:58 +0700

    fix bug

--- smart_contract/chainlist/BoredApeYachtClub/contract_name.py
+++ smart_contract/chainlist/BoredApeYachtClub/contract_name.py
@@ -5,7 +5,8 @@ from web3 import Web3
 w3 = Web3(Web3.HTTPProvider('https://eth.drpc.org'))
 
 contract_address = "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D"
-contract_abi_file = f"{os.getcwd()}/chainlist/BoredApeYachtClub/contract_abi.json"
+current_directory = os.path.dirname(os.path.abspath(__file__))
+contract_abi_file = f"{current_directory}/contract_abi.json"
 with open(contract_abi_file, "r") as json_file:
     abi = json.load(json_file)
 
