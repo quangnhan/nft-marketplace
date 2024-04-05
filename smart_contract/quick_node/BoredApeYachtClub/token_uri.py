8c036f83e93ea088412db61c274cbf396a730c30
Nhan Vo Quang <vqnhan@tma.com.vn>
2024-04-05 15:17:15 +0700

    Add contract abi

--- smart_contract/quick_node/BoredApeYachtClub/token_uri.py
+++ smart_contract/quick_node/BoredApeYachtClub/token_uri.py
@@ -5,7 +5,7 @@ from web3 import Web3
 w3 = Web3(Web3.HTTPProvider('https://silent-powerful-sunset.quiknode.pro/1b4b2ca8e8106b992d01400a71e0ba35810091e2/'))
 
 contract_address = "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D"
-contract_abi_file = f"{os.getcwd()}/quick_node/contract_abi.json"
+contract_abi_file = f"{os.getcwd()}/quick_node/BoredApeYachtClub/contract_abi.json"
 with open(contract_abi_file, "r") as json_file:
     abi = json.load(json_file)
 
